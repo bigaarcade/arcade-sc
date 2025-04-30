5ff4ae322cfd63ff9a86c84f690843f0f340879e
Nhat Tran - DEKON <nhat.tran@lotussoft.vn>
2025-04-30 11:28:40 +0700

    Fix missing Owner config

--- scripts/configs/index.ts
+++ scripts/configs/index.ts
@@ -1,6 +1,6 @@
-import { BIGA, VALIDATOR, WINDOW_DURATION, WITHDRAWAL_LIMIT } from './config.json';
+import { BIGA, OWNER, VALIDATOR, WINDOW_DURATION, WITHDRAWAL_LIMIT } from './config.json';
 import { NAME, SUPPLY, SYMBOL } from './token.json';
 import { BIGA_STAKING, STAKING_TOKEN, STAKING_OWNER } from './staking.json';
-export const BigaConfigs = { BIGA, VALIDATOR, WITHDRAWAL_LIMIT, WINDOW_DURATION };
+export const BigaConfigs = { BIGA, OWNER, VALIDATOR, WITHDRAWAL_LIMIT, WINDOW_DURATION };
 export const TokenConfigs = { NAME, SYMBOL, SUPPLY };
 export const BigaStakingConfigs = { BIGA_STAKING, STAKING_TOKEN, STAKING_OWNER };
